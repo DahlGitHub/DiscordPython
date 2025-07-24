f16ab83ba77c5da5b33bab3692f15a87782bbff7
Adrian Dahl <78036458+DahlGitHub@users.noreply.github.com>
2025-07-24 14:46:27 +0200

    Added Ninjakiwi and Cobblemon

--- utils/icons.py
+++ utils/icons.py
@@ -1,4 +1,5 @@
 ICONS = {
     "riotgames" : "https://cdn.discordapp.com/attachments/1155038652137734236/1392020624423256084/channels4_profile.jpg",
-    "ninjakiwi" : "https://cdn.discordapp.com/attachments/1155038652137734236/1395004944204955729/unnamed.png?ex=6878df18&is=68778d98&hm=93d25ebc044c4b3a90dad4b47d9bcece2e56903151fbaae6b69c9b393e41b6a8&"
+    "ninjakiwi" : "https://cdn.discordapp.com/attachments/1155038652137734236/1395004944204955729/unnamed.png?ex=6878df18&is=68778d98&hm=93d25ebc044c4b3a90dad4b47d9bcece2e56903151fbaae6b69c9b393e41b6a8&",
+    "cobblemon" : "https://cdn.discordapp.com/attachments/1155038652137734236/1397547506467864648/1117423483190788158.webp?ex=68821f0a&is=6880cd8a&hm=6612ef4cdf1242b0b5a14b4838adc0581450e2865393eabf1477f3b8bcdde8db&"
 }
